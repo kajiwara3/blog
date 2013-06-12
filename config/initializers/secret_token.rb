8179c2b7879866012c9595d775719b17d6a1006b
seiji akatsuka <info.akatsuka@gmail.com>
2013-06-12 17:00:19 +0900

    all commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '3f4f80ab48b6186f940e9c42cd9c3d7c525dda1b27220d0643a54d80d9d3580361b131de59b7680bb13469c4b229df5f17256079aaf68f68bcace09c2382062f'
---
+++ config/unicorn.rb
@@ -0,0 +1,35 @@
+# ワーカーの数
+worker_processes 2
+
+# RAILS ROOT
+working_directory ENV['APP_ROOT']
+
+# ソケット経由で通信する
+listen File.expand_path('/tmp/unicorn_chef_rails_template.sock')
+pid File.expand_path('tmp/pids/unicorn.pid', ENV['RAILS_ROOT'])
+
+# ログ
+stderr_path File.expand_path('log/unicorn_stderr.log', ENV['RAILS_ROOT'])
+stdout_path File.expand_path('log/unicorn_stdout.log', ENV['RAILS_ROOT'])
+
+# ダウンタイムなくす
+preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{ server.config[:pid] }.oldbin"
+  unless old_pid == server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      puts "Sending #{sig} signal to old unicorn master..."
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+  #sleep 1
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
+end
